9394415e340d04a7e24247685f7b45c9ef480c6c
Jano <75015989+janoPig@users.noreply.github.com>
2023-01-21 20:14:55 +0100

    Sk learn (#11)
    * sklearn compatibile
    
    * Commit from GitHub Actions (Feynman - linux)
    
    Co-authored-by: janoPig <janoPig@users.noreply.github.com>

--- HROCH/hroch.py
+++ HROCH/hroch.py
@@ -4,50 +4,29 @@ import platform
 from tempfile import TemporaryDirectory
 import subprocess
 import numpy as np
+from sklearn.base import BaseEstimator, RegressorMixin
+from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
 
-def problem_to_string(problem: any):
-    if isinstance(problem, str):
-        if problem == 'simple' or problem == 'math' or problem == 'fuzzy':
-            return problem
-        raise ValueError('Invalid problem type')
-    if not isinstance(problem, dict):
-        raise TypeError('Invalid problem type')
-    result = ""
-    for instr, prob in problem.items():
-        if not isinstance(instr, str) or not isinstance(prob, float):
-            raise TypeError('Invalid instruction type')
-        if len(instr) == 0 or prob < 0.0:
-            raise ValueError('Invalid instruction value')
-        result = result + f'{instr} {prob};'
-    return result
 
-def feature_probs_to_string(feat: any):
-    result = ""
-    if feat is None:
-        return result
-    for prob in feat:
-        result = result + f'{prob};'
-    return result
-
-class PHCRegressor:
+class PHCRegressor(BaseEstimator, RegressorMixin):
     '''Parallel Hill Climbing symbolic regressor'''
 
-    def __init__(self, 
-        num_threads: int = 8,
-        time_limit: float = 5.0,
-        iter_limit: int = 0,
-        stopping_criteria: float = 0.0,
-        precision: str = 'f32',
-        problem: any = 'math',
-        feature_probs: any = None,
-        save_model: bool = False,
-        random_state: int = 0,
-        verbose: bool = False,
-        pop_size : int = 64,
-        pop_sel : int = 4,
-        const_size : int = 8,
-        code_size : int = 32
-    ):
+    def __init__(self,
+                 num_threads: int = 8,
+                 time_limit: float = 5.0,
+                 iter_limit: int = 0,
+                 stopping_criteria: float = 0.0,
+                 precision: str = 'f32',
+                 problem: any = 'math',
+                 feature_probs: any = None,
+                 save_model: bool = False,
+                 random_state: int = 0,
+                 verbose: bool = False,
+                 pop_size: int = 64,
+                 pop_sel: int = 4,
+                 const_size: int = 8,
+                 code_size: int = 32
+                 ):
 
         self.num_threads = num_threads
         self.time_limit = time_limit
@@ -67,6 +46,9 @@ class PHCRegressor:
         self.dir = TemporaryDirectory()
 
     def fit(self, X: np.ndarray, y: np.ndarray):
+
+        X, y = check_X_y(X, y, accept_sparse=False)
+
         fname_x = self.dir.name + '/x.csv'
         fname_y = self.dir.name + '/y.csv'
         fname_model = self.dir.name + '/model.hrx'
@@ -87,7 +69,8 @@ class PHCRegressor:
             cli = path + '/hroch.exe'
 
         process = subprocess.Popen([cli,
-                                    '--problem', problem_to_string(self.problem),
+                                    '--problem', self.__problem_to_string(
+                                        self.problem),
                                     '--task', 'fit',
                                     '--x', fname_x,
                                     '--y', fname_y,
@@ -98,7 +81,8 @@ class PHCRegressor:
                                     '--numThreads', f'{self.num_threads}',
                                     '--stoppingCriteria', f'{self.stopping_criteria}',
                                     '--randomState', f'{self.random_state}',
-                                    '--featProbs', feature_probs_to_string(self.feature_probs),
+                                    '--featProbs', self.__feature_probs_to_string(
+                                        self.feature_probs),
                                     '--popSize', f'{self.pop_size}',
                                     '--popSel', f'{self.pop_sel}',
                                     '--constSize', f'{self.const_size}',
@@ -116,7 +100,12 @@ class PHCRegressor:
         process.stdout.close()
         process.wait()
 
-    def predict(self, X_test: np.ndarray):
+        self.is_fitted_ = True
+
+    def predict(self, X: np.ndarray):
+        check_is_fitted(self)
+        X = check_array(X, accept_sparse=False)
+
         fname_x = self.dir.name + '/x.csv'
         fname_y = self.dir.name + '/y.csv'
         fname_model = self.dir.name + '/model.hrx'
@@ -127,7 +116,7 @@ class PHCRegressor:
         if os.path.exists(fname_y):
             os.remove(fname_y)
 
-        np.savetxt(fname_x, X_test, delimiter=' ')
+        np.savetxt(fname_x, X, delimiter=' ')
 
         path = os.path.dirname(os.path.realpath(__file__))
 
@@ -150,19 +139,29 @@ class PHCRegressor:
         process.stdout.close()
         process.wait()
         y = np.genfromtxt(fname_y, delimiter=' ')
+
         return y
 
-    def get_params(self, deep=True):
-        return {'num_threads': self.num_threads,
-                'time_limit': self.time_limit,
-                'iter_limit': self.iter_limit,
-                'stopping_criteria': self.stopping_criteria,
-                'verbose': self.verbose,
-                'precision': self.precision,
-                'problem': self.problem,
-                'save_model': self.save_model,
-                'random_state': self.random_state,
-                'pop_size': self.pop_size,
-                'pop_sel': self.pop_sel,
-                'const_size': self.const_size,
-                'code_size': self.code_size}
+    def __problem_to_string(self, problem: any):
+        if isinstance(problem, str):
+            if problem == 'simple' or problem == 'math' or problem == 'fuzzy':
+                return problem
+            raise ValueError('Invalid problem type')
+        if not isinstance(problem, dict):
+            raise TypeError('Invalid problem type')
+        result = ""
+        for instr, prob in problem.items():
+            if not isinstance(instr, str) or not isinstance(prob, float):
+                raise TypeError('Invalid instruction type')
+            if len(instr) == 0 or prob < 0.0:
+                raise ValueError('Invalid instruction value')
+            result = result + f'{instr} {prob};'
+        return result
+
+    def __feature_probs_to_string(self, feat: any):
+        result = ""
+        if feat is None:
+            return result
+        for prob in feat:
+            result = result + f'{prob};'
+        return result
