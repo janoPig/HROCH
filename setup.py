188b95938c67604d5ada38201137fc9a1f1af63e
janoPig <jan.pigos@gmail.com>
2024-01-20 19:21:37 +0100

    remove python 3.7, inc version

--- setup.py
+++ setup.py
@@ -52,6 +52,7 @@ yp = reg.predict(X_test)
 
 ### v1.4
 
+- sklearn compatibility
 - Classificators:
   - NonlinearLogisticRegressor for a binary classification
   - SymbolicClassifier for multiclass classification
@@ -91,7 +92,7 @@ yp = reg.predict(X_test)
 
 setup(
     name='HROCH',
-    version='1.4.3',
+    version='1.4.4',
     description='Symbolic regression and classification',
     long_description=ldesc,
     long_description_content_type="text/markdown",
@@ -111,7 +112,6 @@ setup(
                  'License :: OSI Approved :: MIT License',
                  'Programming Language :: Python',
                  'Programming Language :: Python :: 3',
-                 'Programming Language :: Python :: 3.7',
                  'Programming Language :: Python :: 3.8',
                  'Programming Language :: Python :: 3.9',
                  'Programming Language :: Python :: 3.10',
