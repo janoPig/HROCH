725b54c51f16a24ba2c28eba4fb58d20f7fe82ca
janoPig <jan.pigos@gmail.com>
2024-01-28 13:28:11 +0100

    1.4.8 version

--- setup.py
+++ setup.py
@@ -92,7 +92,7 @@ yp = reg.predict(X_test)
 
 setup(
     name='HROCH',
-    version='1.4.7',
+    version='1.4.8',
     description='Symbolic regression and classification',
     long_description=ldesc,
     long_description_content_type="text/markdown",
